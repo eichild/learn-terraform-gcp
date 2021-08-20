477bce0c195035141474dc359cc601bae89dc86d
Lucas Gregorio Martins <lucas.martins@tqi.com.br>
2021-08-19 22:11:47 -0300

    criada vpc_network

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  //credentials = file("<NAME>.json")
+  credentials = file("learn-gcp-323302-36fe78ab72ae.json")
+
+  project = "learn-gcp-323302"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
